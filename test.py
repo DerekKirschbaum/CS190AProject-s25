bf3cacaf9c44510dd4ff4f6e9dc3e0bb70b0f869
Derek Kirschbaum <dgkirschbaum@ucsb.edu>
2025-06-01 22:16:48 -0700

    dk - updates

---
+++ test.py
@@ -0,0 +1,40 @@
+# imports
+from models.simplecnn import SimpleCNN
+from models.vgg import VGG
+from models.casia import Casia
+from perturbations import evaluate_attack
+
+from preprocess_data import TEST_SET
+
+figure_path = './figures/'
+if __name__ == "__main__":
+    # Instantiate and load each model
+    cnn = SimpleCNN()
+    vgg = VGG()
+    #casia = Casia()
+
+    cnn_path = "./checkpoints/simplecnn.npy"
+    vgg_path = "./checkpoints/vgg.npy"
+    #casia_path = "./checkpoints/casia.npy"
+
+    cnn.load(cnn_path)
+    vgg.load(vgg_path)
+    #casia.load(casia_path)
+
+    # Prepare the list of target models and their labels
+    target_models = [cnn, vgg]
+    model_labels  = ["CNN", "VGG"]
+
+    # Define the epsilons to test
+    epsilons = [round(i * 0.05, 2) for i in range(10)]  # [0.0, 0.05, 0.10, ..., 0.45]
+
+
+    evaluate_attack(
+        source_model=cnn,
+        target_models=target_models,
+        model_labels=model_labels,
+        dataset=TEST_SET,
+        epsilons=epsilons,
+        attack_method="pgd",
+        save_path=figure_path
+    )
